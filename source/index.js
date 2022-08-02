543849ad173a460feacd0134abab351217cf2547
henricc <80387601+henricc@users.noreply.github.com>
2022-08-02 14:33:36 +0200

    get userId

--- source/index.js
+++ source/index.js
@@ -1,5 +1,18 @@
+function formatResponse(activeStreams, allowNewStream) {
+
+    const response = {
+        "activeStreams": activeStreams,
+        "allowNewStream": allowNewStream
+    };
+
+    return response;
+}
+
 exports.handler = async (event) => {
-    // TODO implement
+
+    const userId = event.userId;
+    console.log(userId);
+
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
